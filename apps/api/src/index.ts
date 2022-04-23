496e5f54ffb5f1219a917bb7d32fbdaed0c0c56a
tanakaworld <yutaro.tanaka.world@gmail.com>
2022-04-23 22:50:51 +0900

    feat(api): add api app

---
+++ apps/api/src/index.ts
@@ -0,0 +1,47 @@
+import { join } from "path";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import { ApolloServer } from "apollo-server";
+import { Card, Resolvers } from "./types";
+
+const cards: Card[] = [
+  {
+    id: "1",
+    front: "hold one's head up",
+    back: "gain the strength need to hold one's head up",
+  },
+  {
+    id: "2",
+    front: "roll over",
+    back: "turn from lying on one's back to being on one's tummy and vice versa",
+  },
+  {
+    id: "3",
+    front: "crawl",
+    back: "move on one's hands and knees",
+  },
+  {
+    id: "4",
+    front: "sit up",
+    back: "sit independently",
+  },
+];
+
+const schema = loadSchemaSync(join(__dirname, "./graphql/schema.graphql"), {
+  loaders: [new GraphQLFileLoader()],
+});
+
+const resolvers: Resolvers = {
+  Query: {
+    cards: () => cards,
+  },
+};
+
+const server = new ApolloServer({
+  schema: addResolversToSchema({ schema, resolvers }),
+});
+
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`🚀 Server is ready at ${url}`);
+});
