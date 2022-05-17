23c4a80e5efd0586b319832ca0fa251323a1d88d
tanakaworld <yutaro.tanaka.world@gmail.com>
2022-05-17 22:00:00 +0900

    feat(web): display createdAt and updatedAt

--- apps/web/src/components/card-table.tsx
+++ apps/web/src/components/card-table.tsx
@@ -12,14 +12,18 @@ export default function CardTable({ cards }: Props) {
           <th>Id</th>
           <th>Front</th>
           <th>Back</th>
+          <th>CreatedAt</th>
+          <th>UpdatedAt</th>
         </tr>
       </thead>
       <tbody>
-        {cards.map(({ id, front, back }) => (
+        {cards.map(({ id, front, back, createdAt, updatedAt }) => (
           <tr key={id}>
             <td>{id}</td>
             <td>{front}</td>
             <td>{back}</td>
+            <td>{createdAt}</td>
+            <td>{updatedAt}</td>
           </tr>
         ))}
       </tbody>
