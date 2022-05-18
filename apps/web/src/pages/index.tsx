8a7bd33614331a704e0cd1ef1c76a9257a1d5a27
tanakaworld <yutaro.tanaka.world@gmail.com>
2022-05-18 21:57:23 +0900

    feat(web): move query into card-table

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,24 +1,12 @@
-import { QUERY_CARDS } from "~/graphql/query";
 import CardTable from "~/components/card-table";
-import { useQuery } from "@apollo/client";
 
 export default function Index() {
-  const { data, loading, error } = useQuery(QUERY_CARDS);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Failed to load</p>;
-  }
-
   return (
     <div>
       <h1>Web</h1>
 
       <h2>Cards</h2>
-      <CardTable cards={data.cards} />
+      <CardTable />
     </div>
   );
 }
