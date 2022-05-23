dc68fc8877cec114a0e15451b57f77b8cd02b4bd
tanakaworld <yutaro.tanaka.world@gmail.com>
2022-05-23 11:29:21 +0900

    feat(ui): add Table component

---
+++ packages/ui/components/Table.tsx
@@ -0,0 +1,52 @@
+import styles from "@emotion/styled";
+
+type Props = {
+  columns: string[];
+  data: (string | number | (() => string | number))[][];
+};
+
+const STable = styles.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+const STh = styles.th`
+  border: solid 1px var(--primary);
+  padding: var(--space-content-sm);
+`;
+
+const STd = styles.td`
+  border: solid 1px var(--primary);
+  padding: var(--space-content-sm);
+`;
+
+// FIXME avoid using index as key
+export const Table = ({ columns, data }: Props) => {
+  return (
+    <STable>
+      <thead>
+        <tr>
+          {columns.map((c, index) => {
+            const key = `th-${index}`;
+            return <STh key={key}>{c}</STh>;
+          })}
+        </tr>
+      </thead>
+
+      <tbody>
+        {data.map((row, rowIndex) => {
+          const rowKey = `row-${rowIndex}`;
+          return (
+            <tr key={rowKey}>
+              {row.map((col, colIndex) => {
+                const value = typeof col === "function" ? col() : col;
+                const columnKey = `${rowKey}-col-${colIndex}`;
+                return <STd key={columnKey}>{value}</STd>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </STable>
+  );
+};
